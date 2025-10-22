191715a9ef956bdbc95d4efdad0954d1ca0ca733
0x66 <117127775+FlipSkateHd@users.noreply.github.com>
2025-10-21 22:41:12 -0300

    Função de ícone aprimorada
    ícone fica vermelho ao detectar atividade

--- pip.py
+++ pip.py
@@ -10,18 +10,13 @@ import pystray
 PROCESSOS_ALVO = ["veyon-service.exe", "veyon-server.exe", "veyon-worker.exe"]
 LIMITE_MBPS = 0.4  # limite em MB/s
 
-def criar_icone():
-    img = Image.new('RGB', (64, 64), color='blue')
-    d = ImageDraw.Draw(img)
-    d.rectangle([16, 16, 48, 48], fill='white')
-    return img
-
-def abrir_explorer_60s():
-    # abre explorer.exe
-    proc = subprocess.Popen("explorer.exe")
-    # espera 60 segundos
-    time.sleep(60)
-def monitor_rede():
+def criar_icone(cor):
+        img = Image.new('RGB', (64, 64), color=cor)
+        d = ImageDraw.Draw(img)
+        d.rectangle([16, 16, 48, 48], fill=cor)
+        return img
+    
+def monitor_rede(icon):
     last_total = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
     while True:
         time.sleep(1)
@@ -40,10 +35,19 @@ def monitor_rede():
                 # )
                 # abre explorer e fecha depois de 60s em thread separada
                 Thread(target=abrir_explorer_60s, daemon=True).start()
+                icon.icon = criar_icone('red')
+            else:
+                icon.icon = criar_icone('blue')
+
+def abrir_explorer_60s():
+    # abre explorer.exe
+    proc = subprocess.Popen("explorer.exe")
+    # espera 60 segundos
+    time.sleep(60)
 
 def iniciar_tray():
-    icon = pystray.Icon("MonitorRede", criar_icone(), "Monitor de Charles")
+    icon = pystray.Icon("MonitorRede", criar_icone('blue'), "Monitor de Charles")
+    Thread(target=monitor_rede, args=(icon,), daemon=True).start()
     icon.run()
 
-Thread(target=monitor_rede, daemon=True).start()
 iniciar_tray()
